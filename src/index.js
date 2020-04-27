e2078c84e7895fbcdbb9092619e83f274ee7cb5c
kamiyab <kamiyabasamadi@gmail.com>
2020-04-27 15:14:46 +0530

    remove some code

--- src/index.js
+++ src/index.js
@@ -1,17 +1,8 @@
 const Discord = require("discord.js");
 const axios = require("axios");
 require("dotenv").config();
-const http = require("http");
 
 const client = new Discord.Client();
-
-http.createServer((req, res) => {
-    res.writeHead(200, {
-        "Content-type": "text/plain"
-    });
-        res.write("Hey");
-        res.end();
-    }).listen(4000);
 
 client.once("ready",() => {
 	console.log("its ready");
@@ -20,22 +11,25 @@ client.once("ready",() => {
 client.on("message", async message => {
 	if(message.content.includes("!insult")){
         const arry = message.content.split(" ");
-        
-        if(arry[1] === "me"){
+        if(arry.length > 1){
+            if(arry[1] === "me"){
 
-            let userid = message.author.id;
-            
-            userid = "<@!"+userid+">";
-
-            const insult = await getInsult();
-
-            message.channel.send( userid + " "+insult);
-        }else if(arry[1].startsWith("<@!")){
-            const id = arry[1];
-
-            const insult = await getInsult();
-
-            message.channel.send(id + " " +insult);
+                let userid = message.author.id;
+                
+                userid = "<@!"+userid+">";
+    
+                const insult = await getInsult();
+    
+                message.channel.send( userid + " "+insult);
+            }else if(arry[1].startsWith("<@!")){
+                const id = arry[1];
+    
+                const insult = await getInsult();
+    
+                message.channel.send(id + " " +insult);
+            }
+        }else{
+            message.channel.send("Currently in Early stage..");
         }
 	}
 });
