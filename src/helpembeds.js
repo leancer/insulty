90e379373e9be1ad32675b6b06699d099f39c93a
leancer <kamiyabasamadi@gmail.com>
2020-06-17 18:02:15 +0530

    improve helpembed

--- src/helpembeds.js
+++ src/helpembeds.js
@@ -5,7 +5,11 @@ const helpEmbed = new Discord.MessageEmbed();
 helpEmbed.setColor("#0099ff")
 .setTitle("Insulty Help")
 .setThumbnail('https://dashboard.snapcraft.io/site_media/appmedia/2018/05/angry_1.png')
-.setDescription('Find What can You do With Insulty')
+.setDescription('Find What can You do With Insulty \n  Commands list')
+.addFields(
+    { name: '!insult me', value: 'insult yourself', inline: true },
+    { name: '!insult @<user>', value: 'insult someone on server with its username', inline: true },
+)
 .setFooter('Made by LEANCER');
 
 module.exports = helpEmbed;
